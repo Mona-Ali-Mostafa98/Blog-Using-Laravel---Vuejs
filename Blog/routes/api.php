a89596c15482e5ea6d70f99a22f4e8f3da146960
Mona-Ali-Mostafa98 <monaali980628@gmail.com>
2024-05-15 22:18:19 +0300

    Edit

--- Blog/routes/api.php
+++ Blog/routes/api.php
@@ -32,11 +32,7 @@ Route::post('/login', function (Request $request) {
 });
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/posts', [PostController::class, "index"]);
-    Route::post('/posts', [PostController::class, "store"]);
-    Route::get('/posts/{post}', [PostController::class, "show"]);
-    Route::put('/posts/{post}', [PostController::class, "update"]);
-    Route::delete('/posts/{post}', [PostController::class, "destroy"]);
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 });
 
 
